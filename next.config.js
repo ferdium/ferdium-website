0b200f4ee69d99ca6ec05918b9468bca86b1d25e
Nathanaël Houn <contact@nathanaelhoun.fr>
2022-05-08 22:18:00 +0200

    ci: setup github-actions ci

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 const path = require("path");
-const isProd = process.env.NODE_ENV === "production"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.modules.push(path.resolve("./"));
-
     return config;
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  assetPrefix: isProd ? "/website/" : "",
+  assetPrefix: './',
 };
 
 module.exports = nextConfig;
